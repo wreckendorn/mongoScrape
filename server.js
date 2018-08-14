2b1210df54ab239b67e6d5a81d1da7e0e0f6bfca
Hecky <thechrisheckendorn@gmail.com>
2018-08-14 18:43:46 -0500

    what's wrong with you Heroku??

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/News";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
